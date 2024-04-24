c9d429b01ae7edcace70d5491c788dadc00a6302
Mario canedo <Mario_cane.do@hotmail.com>
2024-04-24 11:31:30 -0400

    Commit inicial proyecto

---
+++ app/src/main/java/com/example/librarynavigation/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.librarynavigation;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.librarynavigation.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding mBinding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+    }
+}
